b4bc4b8cc639494a49153ac159fce9943f5120c3
Stephen Lu <munk801@gmail.com>
2013-11-03 21:21:20 -0700

    Added integration with devise and heroku.  Also, added functionality to log-in and sign up buttons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,3 @@
-Pinteresting::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -77,4 +76,9 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Defined from Devise for heroku
+  # TODO switch this from local host to actual production website
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
