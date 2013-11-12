66fb7f6d4ce30ce5ed4af61cea7ef43f1b7cd8ea
Stephen Lu <munk801@gmail.com>
2013-11-11 21:54:58 -0600

    Added information for pins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pinteresting::Application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
